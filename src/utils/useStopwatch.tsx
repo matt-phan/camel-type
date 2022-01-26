568aacddb659d45e7795d07a91d692b74938e480
Matt Phan <matthewmnphan@outlook.com>
2022-01-26 20:46:49 +0000

    creating useStopwatch custom hook

---
+++ src/utils/useStopwatch.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+const useStopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+
+  useEffect(() => {
+    let interval: any = null;
+
+    if (isActive && isPaused === false) {
+      interval = setInterval(() => {
+        setTime((time) => time + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, isPaused]);
+
+  const handleStart = () => {
+    setIsActive(true);
+    setIsPaused(false);
+  };
+
+  const handlePause = () => {
+    setIsPaused(true);
+  };
+
+  const handleResume = () => {
+    setIsPaused(false);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setTime(0);
+  };
+
+  return { time, handleStart, handlePause, handleResume, handleReset };
+};
+
+export default useStopwatch;
